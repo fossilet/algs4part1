61687e4046968cc298855d26c8e6119179a5214b
Yongzhi Pan <panyongzhi@gmail.com>
2014-06-27 17:42:49 +0800

    First working version.
    I solved backwash bug from the aid of
    https://github.com/agjacome/algs4part1-class.

--- src/Percolation.java
+++ src/Percolation.java
@@ -8,7 +8,7 @@ public class Percolation {
     private int vtop;
     private int vbott;
     private int num; // size
-    private int[][] sites;
+    private boolean[][] sites;
     private WeightedQuickUnionUF uf, uf1;
 
     public Percolation(int N) {
@@ -18,17 +18,16 @@ public class Percolation {
             throw new IllegalArgumentException();
         }
         num = N;
-        // FIXME
         vtop = num * num;
-//        vbott = num * num + 1;
+        vbott = num * num + 1;
         // The last two are virtual top and bottom sites.
         uf = new WeightedQuickUnionUF(num * num + 2);
-//        uf1 = new WeightedQuickUnionUF(num * num + 2);
-        sites = new int[num][num];
+        uf1 = new WeightedQuickUnionUF(num * num + 2);
+        sites = new boolean[num][num];
         // initialize sites.
         for (int i = 0; i < num; i++) {
             for (int j = 0; j < num; j++) {
-                sites[i][j] = 0;
+                sites[i][j] = false;
             }
         }
     }
@@ -48,15 +47,8 @@ public class Percolation {
     public void open(int i, int j) {
         validateInd(i, j);
         // open site (row i, column j) if it is not already
-        if (sites[i - 1][j - 1] == 0) {
-            sites[i - 1][j - 1] = 1;
-            // connect to respective virtual site if on top or bottom row.
-            if (i == 1) {
-                uf.union(vtop, xyton(i - 1, j - 1));
-            }
-//            if (i == num) {
-//                uf1.union(vbott, xyton(i - 1, j - 1));
-//            }
+        if (!sites[i - 1][j - 1]) {
+            sites[i - 1][j - 1] = true;
             // adjacent open sites
             // normal coordiates, from 0 to N - 1.
             int in = i - 1;
@@ -67,11 +59,21 @@ public class Percolation {
                 int x = adjSites[k][0];
                 int y = adjSites[k][1];
                 // connect to valid and open neighbors
-                if (0 <= x && x <= num - 1 && 0 <= y && y <= num - 1
-                        && sites[x][y] == 1) {
+                if (0 <= x && x <= num - 1 && 0 <= y && y <= num - 1 && sites[x][y]) {
                     uf.union(xyton(i - 1, j - 1), xyton(x, y));
-//                    uf1.union(xyton(i - 1, j - 1), xyton(x, y));
+                    uf1.union(xyton(i - 1, j - 1), xyton(x, y));
                 }
+            }
+            // connect to respective virtual site if on top or bottom row.
+            if (i == 1) {
+                uf.union(vtop, xyton(i - 1, j - 1));
+                uf1.union(vtop, xyton(i - 1, j - 1));
+            }
+            if (i == num) {
+//            link to virtual bottom site --> percolate but backwash
+//            do not link to virtual bottom site --> do not backwash or
+//            percolate
+                uf.union(vbott, xyton(i - 1, j - 1));
             }
         }
     }
@@ -79,27 +81,21 @@ public class Percolation {
     public boolean isOpen(int i, int j) {
         validateInd(i, j);
         // is site (row i, column j) open?
-        return sites[i - 1][j - 1] == 1;
+        return sites[i - 1][j - 1];
     }
 
     public boolean isFull(int i, int j) {
         validateInd(i, j);
         // is site (row i, column j) full?
         // test if the site is connected with the virtual top site
-        return uf.connected(vtop, xyton(i - 1, j - 1));
+        return isOpen(i, j) && uf1.connected(vtop, xyton(i - 1, j - 1));
     }
 
     public boolean percolates() {
         // does the system percolate?
         // whether the virtual top site is connected to the virtual bottom site.
-        // FIXME: always false: 100 != 101
-//        return uf.find(vtop) == uf1.find(vbott);
-        for (int i = num*(num -1); i < num*num; i++) {
-            if (uf.find(i) == uf.find(vtop)) {
-                return true;
-            }
-        }
-        return false;
+//        return uf.find(vtop) == uf.find(vbott);
+        return uf.connected(vtop, vbott);
     }
 
     public static void main(String[] args) {
